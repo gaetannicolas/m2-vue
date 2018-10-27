85b9eaae367edf7bffc1b4fb208294ca8b999e2c
Clément Ollivier <clement.o2p@gmail.com>
2018-10-27 12:14:32 +0200

    :flashlight: Add styles

--- src/App.vue
+++ src/App.vue
@@ -24,4 +24,26 @@ body {
   width: 100vw;
   background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
 }
+
+.star {
+  position: absolute;
+  background-color: white;
+  border-radius: 100%;
+  height: 4px;
+  width: 4px;
+}
+
+.moon {
+  position: absolute;
+  height: 100px;
+  width: 100px;
+  background-image: url("https://3dexport.com/items/2015/02/20/385337/100201/moon_texture_3d_model_c4d_max_obj_fbx_ma_lwo_3ds_3dm_stl_1258985_o.png");
+  background-size: cover;
+  box-shadow: 0px 0px 12px 4px white;
+  filter: brightness(1.5);
+  z-index: 1;
+  right: 10%;
+  top: 15%;
+  border-radius: 100px;
+}
 </style>
