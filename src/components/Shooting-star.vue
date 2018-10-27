5daeae88adb05357aa8cda07d428fe8192323d8f
Clément Ollivier <clement.o2p@gmail.com>
2018-10-27 13:01:24 +0200

    :question: Setup exercice-4

---
+++ src/components/Shooting-star.vue
@@ -0,0 +1,41 @@
+<template>
+  <div 
+    class="star shooting-star"
+    :style="style"
+  >
+
+  </div>
+</template>
+
+<script>
+import { randomX, randomY } from "../helpers/random";
+
+export default {
+  name: "Star",
+  computed: {
+    randomX() {
+      return randomX();
+    },
+    randomY() {
+      return randomY();
+    },
+    style() {
+      return `
+        top: ${this.randomY}px;
+        left: ${this.randomX}px;
+      `;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.star {
+  position: absolute;
+  background-color: red;
+  border-radius: 100%;
+  height: 6px;
+  width: 6px;
+}
+</style>
+
