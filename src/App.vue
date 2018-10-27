51115133e1635e740cdfdc610a2fa3d6cdd9db92
Clément Ollivier <clement.o2p@gmail.com>
2018-10-27 12:13:52 +0200

    :bulb: Solution exercice 1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
   <div id="app">
-
+    <div class="moon"></div>
+    <div
+      class="star"
+      v-for="star in totalStars"
+      :key="star"
+      :style="`top: ${getRandomY()}px; left: ${getRandomX()}px;`"
+    ></div>
   </div>
 </template>
 
 <script>
+import { randomX, randomY } from "./helpers/random";
+
 export default {
-  name: "app"
+  name: "app",
+  data() {
+    return {
+      totalStars: 200
+    };
+  },
+  methods: {
+    getRandomX() {
+      return randomX();
+    },
+    getRandomY() {
+      return randomY();
+    }
+  }
 };
 </script>
 
@@ -23,5 +44,27 @@ body {
   height: 100vh;
   width: 100vw;
   background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
+}
+
+.star {
+  position: absolute;
+  background-color: white;
+  border-radius: 100%;
+  height: 4px;
+  width: 4px;
+}
+
+.moon {
+  position: absolute;
+  height: 100px;
+  width: 100px;
+  background-image: url("https://3dexport.com/items/2015/02/20/385337/100201/moon_texture_3d_model_c4d_max_obj_fbx_ma_lwo_3ds_3dm_stl_1258985_o.png");
+  background-size: cover;
+  box-shadow: 0px 0px 12px 4px white;
+  filter: brightness(1.5);
+  z-index: 1;
+  right: 10%;
+  top: 15%;
+  border-radius: 100px;
 }
 </style>
