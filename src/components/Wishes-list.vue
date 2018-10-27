2b6004dc2480c8f132bcb46bfedc2795880ccaab
Clément Ollivier <clement.o2p@gmail.com>
2018-10-27 15:49:58 +0200

    :question: Setup exercice 10

---
+++ src/components/Wishes-list.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul class="wishes-list">
+    <li>
+      <span class="wish-author">Username</span>
+      <p class="wish-content">
+        I wish I was a unicorn !
+      </p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "WishesList"
+};
+</script>
+
+<style lang="scss" scoped>
+.wishes-list {
+  position: fixed;
+  right: 10%;
+  bottom: 5%;
+  color: white;
+}
+
+.wish-author {
+  padding-bottom: 0.5rem;
+  border-bottom: 1px solid white;
+}
+
+.wish-content {
+  padding-bottom: 0.5rem;
+  border-bottom: 1px dashed white;
+}
+</style>
