bb4c916044e72a486d294199b8d807c9a1d75c1c
Clément Ollivier <clement.o2p@gmail.com>
2018-10-27 12:42:08 +0200

    :bulb: Solution exercice 3

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <div id="app">
-    <div class="moon"></div>
+    <Moon />
     <transition-group name="fade">
-      <div
-        class="star"
+      <Star 
         v-for="star in totalStars"
         :key="star"
-        :style="`top: ${getRandomY()}px; left: ${getRandomX()}px;`"
-      ></div>
+      />
     </transition-group>
     <input 
       type="range"
@@ -20,22 +18,19 @@
 </template>
 
 <script>
-import { randomX, randomY } from "./helpers/random";
+import Star from "@/components/Star";
+import Moon from "@/components/Moon";
 
 export default {
   name: "app",
+  components: {
+    Star,
+    Moon
+  },
   data() {
     return {
       totalStars: 200
     };
-  },
-  methods: {
-    getRandomX() {
-      return randomX();
-    },
-    getRandomY() {
-      return randomY();
-    }
   }
 };
 </script>
@@ -53,28 +48,6 @@ body {
   height: 100vh;
   width: 100vw;
   background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
-}
-
-.star {
-  position: absolute;
-  background-color: white;
-  border-radius: 100%;
-  height: 4px;
-  width: 4px;
-}
-
-.moon {
-  position: absolute;
-  height: 100px;
-  width: 100px;
-  background-image: url("https://3dexport.com/items/2015/02/20/385337/100201/moon_texture_3d_model_c4d_max_obj_fbx_ma_lwo_3ds_3dm_stl_1258985_o.png");
-  background-size: cover;
-  box-shadow: 0px 0px 12px 4px white;
-  filter: brightness(1.5);
-  z-index: 1;
-  right: 10%;
-  top: 15%;
-  border-radius: 100px;
 }
 
 /* Transitions */
---
+++ src/components/Star.vue
@@ -0,0 +1,44 @@
+<template>
+  <div 
+    class="star"
+    :style="style"
+  >
+
+  </div>
+</template>
+
+<script>
+import { randomX, randomY } from "../helpers/random";
+
+export default {
+  name: "Star",
+  computed: {
+    randomX() {
+      return randomX();
+    },
+    randomY() {
+      return randomY();
+    },
+    randomRadius() {
+      return Math.round(Math.random() * Math.round(4));
+    },
+    style() {
+      return `
+        top: ${this.randomY}px;
+        left: ${this.randomX}px;
+        height: ${this.randomRadius}px;
+        width: ${this.randomRadius}px;
+      `;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.star {
+  position: absolute;
+  background-color: white;
+  border-radius: 100%;
+}
+</style>
+
