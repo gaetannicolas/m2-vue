42792164b51badab65dcf1ae81891c331862f599
Clément Ollivier <clement.o2p@gmail.com>
2018-10-27 13:17:16 +0200

    :bulb: Solution exercice 5

--- src/components/Shooting-star.vue
+++ src/components/Shooting-star.vue
@@ -2,6 +2,7 @@
   <div 
     class="star shooting-star"
     :style="style"
+    ref="star"
   >
 
   </div>
@@ -25,17 +26,39 @@ export default {
         left: ${this.randomX}px;
       `;
     }
+  },
+  mounted() {
+    let num = Math.floor(Math.random() * 600) + 1; // this will get a number between 1 and 99;
+    num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1; // this will add minus sign in 50% of cases
+    const starDirection = `${num}px`;
+    this.$refs.star.style.setProperty("--star-direction", starDirection);
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.star {
+:root {
+  --star-direction: 300px;
+}
+
+.shooting-star {
   position: absolute;
-  background-color: red;
+  left: 0 !important;
+  height: 6px !important;
+  width: 6px !important;
+  background-color: white;
   border-radius: 100%;
-  height: 6px;
-  width: 6px;
+  animation: move 6s;
+  box-shadow: 0px 0px 2px 1px white;
+}
+
+@keyframes move {
+  from {
+    transform: translate(0, 0);
+  }
+  to {
+    transform: translate(1800px, var(--star-direction));
+  }
 }
 </style>
 
