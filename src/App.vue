4f427dd967739f55b326aa95210f7d88e17150aa
Gaétan Nicolas <gnicolas@appstud.com>
2019-10-11 11:39:46 +0200

    ex 2 + 3

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,33 @@
 <template>
   <div id="app">
-    <div class="moon"></div>
-    <div
-      class="star"
-      v-for="star in totalStars"
-      :key="star"
-      :style="`top: ${getRandomY()}px; left: ${getRandomX()}px;`"
-    ></div>
+    <input type="range" max="100" min="0" v-model.number="totalStars"/>
+    <moon />
+    <transition-group name="fade">
+      <div
+        v-for="star in totalStars"
+        :key="star"
+      >
+        <star />
+      </div>
+    </transition-group>
   </div>
 </template>
 
 <script>
-import { randomX, randomY } from "./helpers/random";
+import Moon from './components/Moon'
+import Star from './components/Star'
 
 export default {
   name: "app",
+  components: {
+    Moon,
+    Star,
+  },
   data() {
     return {
-      totalStars: 200
+      totalStars: 100
     };
   },
-  methods: {
-    getRandomX() {
-      return randomX();
-    },
-    getRandomY() {
-      return randomY();
-    }
-  }
 };
 </script>
 
@@ -46,25 +46,10 @@ body {
   background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
 }
 
-.star {
-  position: absolute;
-  background-color: white;
-  border-radius: 100%;
-  height: 4px;
-  width: 4px;
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
 }
-
-.moon {
-  position: absolute;
-  height: 100px;
-  width: 100px;
-  background-image: url("https://3dexport.com/items/2015/02/20/385337/100201/moon_texture_3d_model_c4d_max_obj_fbx_ma_lwo_3ds_3dm_stl_1258985_o.png");
-  background-size: cover;
-  box-shadow: 0px 0px 12px 4px white;
-  filter: brightness(1.5);
-  z-index: 1;
-  right: 10%;
-  top: 15%;
-  border-radius: 100px;
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
---
+++ src/components/Star.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="star" :style="`top: ${getRandomY()}px; left: ${getRandomX()}px;`" />
+</template>
+
+<script>
+import { randomX, randomY } from "../helpers/random";
+
+export default {
+  name: "Star",
+  methods: {
+    getRandomX() {
+      return randomX();
+    },
+    getRandomY() {
+      return randomY();
+    },
+  }
+}
+</script>
+
+<style>
+
+.star {
+  position: absolute;
+  background-color: white;
+  border-radius: 100%;
+  height: 4px;
+  width: 4px;
+}
+</style>
